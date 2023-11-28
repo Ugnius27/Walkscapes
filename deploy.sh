c8494c84f5ebec64575fdf9f0deea69fe97ae604
Ugnius <ugniusta@proton.me>
2023-11-28 19:21:34 +0200

    Update to deploy script (still terrible)

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 ssh ugnsta@158.129.1.132 "rm -rf walkscapes && mkdir walkscapes && cd walkscapes && mkdir backend"
 scp -r frontend ugnsta@158.129.1.132:/home/ugnsta/walkscapes
 scp backend/target/x86_64-unknown-linux-gnu/release/walkscapes ugnsta@158.129.1.132:/home/ugnsta/walkscapes/backend
+ssh ugnsta@158.129.1.132 "sudo systemctl restart walkscapes"
